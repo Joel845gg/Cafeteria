43c71aff67640caefe4c4b7bd78dd2c42b568b4a
Joel Taipicana <joeltaipicanaro@gmail.com>
2025-12-15 19:53:09 -0500

    Arreglado: carpeta cafeteria ahora es normal

---
+++ Cafeteria/cafeteria-frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Menu from './Menu';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Bienvenido a la Cafetería</h1>
+      <Menu />
+    </div>
+  );
+}
+
+export default App;
---
+++ Cafeteria/cafeteria-frontend/src/Menu.js
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+function Menu() {
+  const [menu, setMenu] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/menu')
+      .then(response => {
+        setMenu(response.data);
+      })
+      .catch(error => {
+        console.error('Error al obtener el menú:', error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>Menú de la Cafetería</h1>
+      <ul>
+        {menu.map(item => (
+          <li key={item.id}>{item.nombre} - ${item.precio}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Menu;
